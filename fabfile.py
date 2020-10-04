d4b286b8f7beae9fd82061cdb434b111fd8f5efe
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-10-04 18:35:27 +0100

    Fix fabfile with autodetection of project name

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,14 @@
+import pathlib
 from fabric.api import local, cd, run, env, prefix
 
+PROJECT = pathlib.Path('.').parent.absolute().parts[-1]
 env.hosts = ['cloud']
 
 
 def deploy():
     local('git push')
-    with prefix('source ~/.virtualenvs/aprendepython/bin/activate'):
-        with cd('~/code/aprendepython'):
+    with prefix(f'source ~/.virtualenvs/{PROJECT}/bin/activate'):
+        with cd(f'~/code/{PROJECT}'):
             run('git pull')
             run('pip install -r requirements.txt')
             run('make dirhtml')
