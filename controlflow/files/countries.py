7434a19c1478da2d810689221fc2974e0b36e76c
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-05-10 19:25:20 +0100

    Add extra exercises about conditionals

---
+++ controlflow/files/countries.py
@@ -0,0 +1,14 @@
+country = 'Italia'
+
+if country == 'Italia':
+    flag = '🇮🇹'
+elif country == 'Rusia':
+    flag = '🇷🇺'
+elif country == 'Taliandia':
+    flag = '🇹🇭'
+elif country == 'Suecia':
+    flag = '🇸🇪'
+else:
+    flag = '🏳'
+
+print(flag)
